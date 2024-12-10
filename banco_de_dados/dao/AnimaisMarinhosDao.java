1f162e34e0dd1e3f1922b4011aa35d15e09bf5d1
Taynara <taynaradasilva2001@gmail.com>
2024-12-10 16:46:29 -0300

    Co-authored-by: Lais Mayer <LaisMayer@users.noreply.github.com>

--- banco_de_dados/dao/AnimaisMarinhosDao.java
+++ banco_de_dados/dao/AnimaisMarinhosDao.java
@@ -12,7 +12,7 @@ import banco_de_dados.conexao.Conexao;
 import banco_de_dados.entity.AnimaisMarinhos;
 
 public class AnimaisMarinhosDao {
-    public void inserirAviao(AnimaisMarinhos animal) {
+    public void inserirAnimal(AnimaisMarinhos animal) {
         String sql= "INSERT INTO animal(nomeEspecie, idade, qtdPatas, peso, profundidadeMax, velocidadeMax) VALUES (?, ?, ?, ?, ?, ?)";
 
         try (Connection connection = new Conexao().conectar()) {
@@ -26,14 +26,14 @@ public class AnimaisMarinhosDao {
             stmt.setInt(6, animal.getVelocidadeMax());
 
             stmt.executeUpdate();
-            System.out.println("Avião inserido com sucesso");
+            System.out.println("Animal inserido com sucesso");
         } catch (SQLException e) {
-            System.out.println("erro ao inserir");
+            System.out.println("Erro ao inserir animal");
             e.printStackTrace();
         }
     }
 
-    public List<AnimaisMarinhos> listarAvioes() {
+    public List<AnimaisMarinhos> listarAnimais() {
         List<AnimaisMarinhos> animaisMarinhos = new ArrayList<>();
         String sql = "SELECT * FROM animal";
 
@@ -67,7 +67,7 @@ public class AnimaisMarinhosDao {
         return animaisMarinhos;
     }
 
-    public AnimaisMarinhos buscarAviao(int idUser) {
+    public AnimaisMarinhos buscarAnimal(int idUser) {
         AnimaisMarinhos animal = new AnimaisMarinhos();
 
         String sql = "SELECT * FROM animal WHERE animal.id = ?";
@@ -104,8 +104,8 @@ public class AnimaisMarinhosDao {
         return animal;
     }
 
-    public void editarModeloAviao(int idUser, String modelo) {
-        String sql = "UPDATE aviao SET modelo = ? WHERE id = ?";
+    public void editarNomeEspecie(int idUser, String modelo) {
+        String sql = "UPDATE animal SET nomeEspecie = ? WHERE id = ?";
 
         try (Connection connection = new Conexao().conectar()) {
             PreparedStatement stmt = connection.prepareStatement(sql);
@@ -124,8 +124,8 @@ public class AnimaisMarinhosDao {
         }
     }
 
-    public void editarFabricanteAviao(int idUser, String fabricante) {
-        String sql = "UPDATE aviao SET fabricante = ? WHERE id = ?";
+    public void editarIdade(int idUser, String fabricante) {
+        String sql = "UPDATE animal SET idade = ? WHERE id = ?";
 
         try (Connection connection = new Conexao().conectar()) {
             PreparedStatement stmt = connection.prepareStatement(sql);
@@ -140,8 +140,8 @@ public class AnimaisMarinhosDao {
         }
     }
 
-    public void editarQtdAssentoAviao(int idUser, int qtdAssento) {
-        String sql = "UPDATE aviao SET qtdAcentos = ? WHERE id = ?";
+    public void editarQtdPatas(int idUser, int qtdAssento) {
+        String sql = "UPDATE animal SET qtdPatas = ? WHERE id = ?";
 
         try (Connection connection = new Conexao().conectar()) {
             PreparedStatement stmt = connection.prepareStatement(sql);
@@ -156,8 +156,8 @@ public class AnimaisMarinhosDao {
         }
     }
 
-    public void editarAnoFabricacaoAviao(int idUser, int anoFabricacao) {
-        String sql = "UPDATE aviao SET anoFabricacao = ? WHERE id = ?";
+    public void editarPeso(int idUser, int anoFabricacao) {
+        String sql = "UPDATE animal SET peso = ? WHERE id = ?";
 
         try (Connection connection = new Conexao().conectar()) {
             PreparedStatement stmt = connection.prepareStatement(sql);
@@ -167,13 +167,45 @@ public class AnimaisMarinhosDao {
 
             stmt.executeUpdate();
         } catch (SQLException e) {
-            System.out.println("Erro ao atualizar aviao");
+            System.out.println("Erro ao atualizar animal");
             e.printStackTrace();
         }
     }
 
-    public void deletarAviao(int idUser) {
-        String sql = "DELETE FROM aviao WHERE id = ?";
+    public void editarProfundidadeMax(int idUser, int anoFabricacao) {
+        String sql = "UPDATE animal SET profundidadeMax = ? WHERE id = ?";
+
+        try (Connection connection = new Conexao().conectar()) {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            
+            stmt.setInt(1, anoFabricacao);
+            stmt.setInt(2, idUser);
+
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Erro ao atualizar animal");
+            e.printStackTrace();
+        }
+    }
+
+    public void editarVelocidadeMax(int idUser, int anoFabricacao) {
+        String sql = "UPDATE animal SET velocidadeMax = ? WHERE id = ?";
+
+        try (Connection connection = new Conexao().conectar()) {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            
+            stmt.setInt(1, anoFabricacao);
+            stmt.setInt(2, idUser);
+
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Erro ao atualizar animal");
+            e.printStackTrace();
+        }
+    }
+
+    public void deletarAnimal(int idUser) {
+        String sql = "DELETE FROM animal WHERE id = ?";
 
         try (Connection connection = new Conexao().conectar()) {
             PreparedStatement stmt = connection.prepareStatement(sql);
@@ -183,16 +215,16 @@ public class AnimaisMarinhosDao {
             int mudanca = stmt.executeUpdate();
 
             if (mudanca > 0) {
-                System.out.println("Tudo certo ao deletar aviao");
+                System.out.println("Tudo certo ao deletar animal");
             }
         } catch (SQLException e) {
-            System.out.println("Erro ao deletar aviao");
+            System.out.println("Erro ao deletar animal");
             e.printStackTrace();
         }
     }
 
     public void contarAvioes() {
-        String sql = "SELECT count(*) FROM aviao";
+        String sql = "SELECT count(*) FROM animal";
 
         try (Connection connection = new Conexao().conectar()) {
             PreparedStatement stmt = connection.prepareStatement(sql);
@@ -202,68 +234,76 @@ public class AnimaisMarinhosDao {
 
             System.out.println(qtd);
         } catch (SQLException e) {
-            System.out.println("Erro ao contar aviao");
+            System.out.println("Erro ao contar animal");
             e.printStackTrace();
         }
     }
 
-    public AnimaisMarinhos buscarAviaoMaisNovo() {
-        String sql = "SELECT * FROM aviao ORDER BY anoFabricacao desc";
-        AnimaisMarinhos aviao = new AnimaisMarinhos();
+    public AnimaisMarinhos buscarAnimalMaisNovo() {
+        String sql = "SELECT * FROM animal ORDER BY anoFabricacao desc";
+        AnimaisMarinhos animal = new AnimaisMarinhos();
 
         try (Connection connection = new Conexao().conectar()) {
             PreparedStatement stmt = connection.prepareStatement(sql);
             ResultSet rs = stmt.executeQuery(sql);
 
             if (rs.next()) {
-                String modelo = rs.getString("modelo");
-                String fabricante = rs.getString("fabricante");
-                int qtdAssento = rs.getInt("qtdAssentos");
-                int anoFabricacao = rs.getInt("anoFabricacao");
+                String nomeEspecie = rs.getString("nomeEspecie");
+                int idade = rs.getInt("idade");
+                int qtdPatas = rs.getInt("qtdPatas");
+                int peso = rs.getInt("peso");
+                int profundidadeMax = rs.getInt("profundidadeMax");
+                int velocidadeMax = rs.getInt("velocidadeMax");
     
-                aviao.setModelo(modelo);
-                aviao.setFabricante(fabricante);
-                aviao.setQtdAssentos(qtdAssento);
-                aviao.setAnoFabricacao(anoFabricacao);
+                animal.setNomeEspecie(nomeEspecie);
+                animal.setIdade(idade);
+                animal.setQtdPatas(qtdPatas);
+                animal.setPeso(peso);
+                animal.setProfundidadeMax(profundidadeMax);
+                animal.setVelocidadeMax(velocidadeMax);
             } else {
-                System.out.println("Não há um avião com esse id");
+                System.out.println("Não há um animal com esse id");
             }
         } catch (SQLException e) {
-            System.out.println("Erro ao contar aviao");
+            System.out.println("Erro ao contar animais");
             e.printStackTrace();
         }
 
-        return aviao;
+        return animal;
     }
 
-    public void buscarAviaoPorIntervalo(int anoMin, int anoMax) {
-        String sql = "SELECT * FROM aviao WHERE aviao.anoFabricacao < ? AND aviao.anoFabricacao > ?";
-        AnimaisMarinhos aviao = new AnimaisMarinhos();
+    public void buscarAnimalPorIntervalo(int idadeMin, int idadeMax) {
+        String sql = "SELECT * FROM animal WHERE aviao.anoFabricacao < ? AND aviao.anoFabricacao > ?";
+        AnimaisMarinhos animal = new AnimaisMarinhos();
 
         try (Connection connection = new Conexao().conectar()) {
             PreparedStatement stmt = connection.prepareStatement(sql);
             
-            stmt.setInt(1, anoMax);
-            stmt.setInt(2, anoMin);
+            stmt.setInt(1, idadeMax);
+            stmt.setInt(2, idadeMin);
 
             ResultSet rs = stmt.executeQuery();
 
             while (rs.next()) {
-                String modelo = rs.getString("modelo");
-                String fabricante = rs.getString("fabricante");
-                int qtdAssento = rs.getInt("qtdAssentos");
-                int anoFabricacao = rs.getInt("anoFabricacao");
+                String nomeEspecie = rs.getString("nomeEspecie");
+                int idade = rs.getInt("idade");
+                int qtdPatas = rs.getInt("qtdPatas");
+                int peso = rs.getInt("peso");
+                int profundidadeMax = rs.getInt("profundidadeMax");
+                int velocidadeMax = rs.getInt("velocidadeMax");
     
-                aviao.setModelo(modelo);
-                aviao.setFabricante(fabricante);
-                aviao.setQtdAssentos(qtdAssento);
-                aviao.setAnoFabricacao(anoFabricacao);
+                animal.setNomeEspecie(nomeEspecie);
+                animal.setIdade(idade);
+                animal.setQtdPatas(qtdPatas);
+                animal.setPeso(peso);
+                animal.setProfundidadeMax(profundidadeMax);
+                animal.setVelocidadeMax(velocidadeMax);
 
-                System.out.println(aviao);
+                System.out.println(animal);
             }
             
         } catch (SQLException e) {
-            System.out.println("Erro ao buscar aviao");
+            System.out.println("Erro ao buscar animal");
             e.printStackTrace();
         }
     }
---
+++ view/AnimaisMarinhosView.java
@@ -0,0 +1,41 @@
+package view;
+public class AnimaisMarinhosView{
+
+    public int imprimirMenu(){
+        return 0;
+    }
+
+    public void lerAnimalMarinho(){
+
+    }
+
+    public void imprimirAnimalMarinho(){
+
+    }
+
+    public void imprimirListaAnimal(){
+
+    }
+
+    public void buscarAnimalPorNome(){
+
+    }
+
+    public void removerAnimalMarinho(){
+
+    }
+
+    public void ordenarAnimalPorNome(){
+
+    }
+
+    public void editarAnimalMarinho(){
+
+
+    }
+
+    public void ContarQtdAnimalMarinho(){
+        
+    }
+
+}
